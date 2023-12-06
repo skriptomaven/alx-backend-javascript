1756dcfab118aaa4b2e1d2b34e413341be8d1a21
papaskripto <bygeoffreyouma@gmail.com>
2023-12-06 22:28:38 +0300

    Create class Student

---
+++ 0x04-Typescript/task_0/js/main.ts
@@ -0,0 +1,13 @@
+class Student {
+	firstName: string;
+	lastName: string;
+	age: number;
+	location: string;
+
+	constructor(firstName: string, lastName: string, age: number, location: string) {
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.age = age;
+		this.location = location;
+	}
+}
