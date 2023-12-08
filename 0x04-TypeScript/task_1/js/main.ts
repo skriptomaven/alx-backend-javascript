9ed9c151ccc29a452141c1d47ddf109ccead4365
papaskripto <bygeoffreyouma@gmail.com>
2023-12-08 14:33:23 +0300

    Add task one file in progress

---
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -0,0 +1,18 @@
+interface Teacher {
+	firstName: string,
+	lastName: string,
+	fullTimeEmployee: boolean,
+	yearsOfExperience: number,
+	location: string,
+	contract: boolean
+}
+
+const teacher: Teacher = {
+	firstName: "John",
+	lastName: "Doe",
+	location: "Nairobi",
+	contract: "false",
+	fullTimeEmployee: false,
+};"
+
+console.log(teacher);
